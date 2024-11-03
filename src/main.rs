d004c8f6c8aecc21c72142da427720e6d31a66b8
Justin Korkie <justinjkorkie@gmail.com>
2024-11-03 11:52:14 +0200

    add wildcard support

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        print!("Zmove expects 2 arguments\n");
+    if args.len() < 3 {
+        print!("Zmove expects at least 2 arguments\n");
         return;
     }
 
-    let file_to_move = &args[1];
-    let frag_of_dir_to_move_to = &args[2];
+    let files_to_move = &args[1..&args.len() - 1];
+    let destination = &args[&args.len() - 1];
+    let files_to_move_as_string = files_to_move.join(" ");
 
     // Check if current directory exists
     let list_to_check_cmd_string = if cfg!(target_os = "windows") {
@@ -21,22 +22,28 @@ fn main() {
         .arg("-c")
         .arg(list_to_check_cmd_string)
         .output()
-        .expect("failed to return to original directory");
+        .expect("An issue occured when trying to find the directory");
     let string_to_check = String::from_utf8_lossy(&list_to_check_cmd.stdout);
-    if string_to_check.contains(&args[2]) {
-        let move_to_dir_cmd_string = if cfg!(target_os = "windows") {
-            format!("move {} {}", &args[1], &args[2])
+
+    //Move files if current directory exists
+    if string_to_check.contains(destination) {
+        let platform_cmd = if cfg!(target_os = "windows") {
+            "move"
         } else {
-            format!("mv {} {}", &args[1], &args[2])
+            "mv"
         };
+        let move_to_dir_cmd_string = format!(
+            "{} {} {}",
+            platform_cmd, files_to_move_as_string, destination
+        );
 
         let move_to_dir_cmd = Command::new("sh")
             .arg("-c")
             .arg(move_to_dir_cmd_string)
             .output()
-            .expect("failed to return to original directory");
+            .expect("An issue occured when moving the files");
         if move_to_dir_cmd.status.success() {
-            println!("Moved {} to {}\n", &args[1], args[2]);
+            println!("Moved {:?} to {}\n", files_to_move, destination);
         } else {
             print!("File not recognized\n");
         }
@@ -44,32 +51,33 @@ fn main() {
     }
 
     // Get availible zoxide directories
-    let move_to_this_cmd_string = format!("zoxide query {}", frag_of_dir_to_move_to.trim());
+    let move_to_this_cmd_string = format!("zoxide query {}", destination.trim());
     let move_to_dir_cmd = Command::new("sh")
         .arg("-c")
         .arg(move_to_this_cmd_string.clone())
         .output()
-        .expect("failed to return to original directory");
+        .expect("Failed to find to original directory");
     let dir_to_move_to = String::from_utf8_lossy(&move_to_dir_cmd.stdout);
 
-    // Move file
+    // Move file(s) to specified zoxide directory
     if move_to_dir_cmd.status.success() {
         let mv_cmd_string = if cfg!(target_os = "windows") {
-            format!("move {} {}", file_to_move.trim(), dir_to_move_to)
+            format!("move {} {}", files_to_move_as_string, dir_to_move_to)
         } else {
-            format!("mv {} {}", file_to_move.trim(), dir_to_move_to)
+            format!("mv {} {}", files_to_move_as_string, dir_to_move_to)
         };
         let mv_cmd = Command::new("sh")
             .arg("-c")
             .arg(mv_cmd_string)
             .output()
-            .expect("failed to return to original directory");
+            .expect("Failed to move files");
 
         if mv_cmd.status.success() {
-            println!("Moved {} to {}\n", file_to_move, dir_to_move_to);
-        } else {
-            print!("File not recognized\n");
+            println!("Moved {} to {}\n", files_to_move_as_string, dir_to_move_to);
+            return;
         }
+        print!("File not recognized\n");
+        return;
     } else {
         print!("Path not recognized\n");
     }
